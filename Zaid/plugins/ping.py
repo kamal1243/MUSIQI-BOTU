f26b3a509d1b96e0bb6486c378d4e2ec9023a2f1
kamal1243 <118734199+kamal1243@users.noreply.github.com>
2022-12-06 16:19:48 +0400

    Update ping.py

--- Zaid/plugins/ping.py
+++ Zaid/plugins/ping.py
@@ -36,5 +36,5 @@ async def _(event):
    delta_ping = time() - start
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   UMM = [[Button.url("⚜ 👨‍💻 DEVELOPER ⚜", "@RafaelGray_DARKWEB")]]
+   UMM = [[Button.url("⚜ 👨‍💻 DEVELOPER ⚜", "https://t.me/RafaelGray_DARKWEB")]]
    await event.reply(f"╰☞ 𝗣𝗢𝗡𝗚™╮\n☞ {delta_ping * 1000:.3f}\n☞ {uptime}", buttons=UMM)
